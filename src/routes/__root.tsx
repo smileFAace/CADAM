a43cd092fb28aaabfc683b007ab40641da211733
“zachdive <zach_dive@berkeley.edu>
2026-05-17 18:10:15 -0700

    Use router head for CADAM title

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -6,6 +6,9 @@ const assetUrl = (path: string) =>
   `${import.meta.env.BASE_URL.replace(/\/?$/, '/')}${path.replace(/^\//, '')}`;
 
 export const Route = createRootRoute({
+  head: () => ({
+    meta: [{ title: 'CADAM' }],
+  }),
   component: RootComponent,
   errorComponent: ({ error }) => (
     <RootDocument>
@@ -26,7 +29,6 @@ function RootDocument({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
-        <title>CADAM</title>
         <link rel="icon" type="image/x-icon" href={assetUrl('adam-icon.ico')} />
         <HeadContent />
       </head>
